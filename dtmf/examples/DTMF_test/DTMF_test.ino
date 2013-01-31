8d103c693949ce31f218e9f68f2dd40f94222769
joseole <juans455@gmail.com>
2013-01-31 11:37:47 +0800

    added return

---
+++ dtmf/examples/DTMF_test/DTMF_test.ino
@@ -0,0 +1,23 @@
+/*
+  DTMF.cpp - TEST code for DTMF library
+*/
+
+
+
+#include "dtmf.h"
+
+DTMF dtmf; // default set to DFRobot DTMF board pins, Speaker pins
+
+void setup()
+{
+ Serial.begin(9600);
+}
+
+void loop()
+{
+  // Play a default number with all possibilities
+  //dtmf.playDTMF();
+  
+  // Read DTMF codes one by one and print it on Serial
+  dtmf.getDTMF();
+}
